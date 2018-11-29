82e136ac9866ea3df5d14203b95ea1ef20f3790e
Bánka Milán <bankamilan22@gmail.com>
2018-11-29 01:25:03 +0100

    Question class update
    Question propertyk felvétele

--- RFTProject/app/src/main/java/com/example/milan/rftproject/Question.java
+++ RFTProject/app/src/main/java/com/example/milan/rftproject/Question.java
@@ -21,5 +21,38 @@ private int id;
         this.wrong3 = wrong3;
         this.correct = correct;
     }  
+    protected String getQuestion()
+    {
+        return question;
+    }
 
+    protected String getDifficulty()
+    {
+        return difficulty;
+    }
+
+    protected String getWrong1()
+    {
+        return wrong1;
+    }
+
+    protected String getWrong2()
+    {
+        return wrong2;
+    }
+
+    protected String getWrong3()
+    {
+        return wrong3;
+    }
+
+    protected String getCorrect()
+    {
+        return correct;
+    }
+
+    protected String getCategory()
+    {
+        return category;
+    }
 }
