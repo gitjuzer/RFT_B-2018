d3cd6fe4511337637addbf703d976b31da8e3da0
slezia <fiberpower.crack@gmail.com>
2018-12-12 23:01:41 +0100

    Stíluslap
    Stíluslap hozzáadás

--- web/view/header.php
+++ web/view/header.php
@@ -5,6 +5,7 @@ require_once 'config.php';
 
 	<head>
 		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
+		
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
                 <title><?php print($sitename) ?></title>
 	</head>
