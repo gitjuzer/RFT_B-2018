8a467e6f39b44d8326116e26afdcf91154a076b7
slezia <fiberpower.crack@gmail.com>
2018-12-12 22:59:37 +0100

    Adatbázis létrehozó lekérdezés
    dektivilás

--- web/config.php
+++ web/config.php
@@ -3,9 +3,10 @@
 	$conn = new PDO('sqlite:db/db_member.sqlite3');
 	
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
-	$query = "CREATE TABLE IF NOT EXISTS member(mem_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT, password TEXT, email TEXT, vezeteknev TEXT, keresztnev TEXT)";
-	
+	/*
+	//erre akkor van szükség h nem létezik az adatbázis fájl
+	$query = "CREATE TABLE IF NOT EXISTS user(mem_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT, password TEXT, email TEXT, vezeteknev TEXT, keresztnev TEXT)";
+	*/
 	$conn->exec($query);
         
 //Alap konfigurációs adatok
